265d5bd8a2763b2f8a77c1ec35ba977532c2db2f
Pouch-dev <diemphuoc5555@gmail.com>
2021-10-07 10:38:50 +0700

    diemphuoc_update_07/10/2021

---
+++ Spring-batch-multithreading/src/main/java/com/example/Springbatchmultithreading/SpringBatchMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbatchmultithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchMultithreadingApplication.class, args);
+	}
+
+}
---
+++ Spring-batch-multithreading/src/test/java/com/example/Springbatchmultithreading/SpringBatchMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbatchmultithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
